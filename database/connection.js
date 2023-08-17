37429a614ce6ec126876e60ff7b699dac184b92f
Ishan Azad <ishansazad@gmail.com>
2023-08-16 22:06:54 -0700

    added dotenv

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect('mongodb+srv://The_Mega_Force:bread@sei.xgav9i3.mongodb.net/item-manager');
+const DATABASE_URL = process.env.DATABASE_URL;
+
+mongoose.connect(DATABASE_URL);
 
 mongoose.connection
 .on("connected", () => console.log("Connected to Mongoose"))
